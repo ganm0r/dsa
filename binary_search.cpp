8d490f83cd25a27b3df30c1b8a9de0670d531361
ganm0r <gndhrvmv@gmail.com>
2025-01-03 00:16:29 +0530

    update the name for binary search

--- binary_search.cpp
+++ binary_search.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int search (vector<int>& n, int t) {
+int binary_search (vector<int>& n, int t) {
     int l = 0, r = n.size() - 1;
     while (l <= r) {
         int m = l + (r - l) / 2;
@@ -19,6 +19,6 @@ int search (vector<int>& n, int t) {
 int main() {
     vector<int> n = { -1, 0, 3, 5, 9, 12 };
     int t = -1;
-    cout << search(n, t) << "\n";
+    cout << binary_search(n, t) << "\n";
     return 0;
 }
