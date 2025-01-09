a72667b75215d482b9f1a39a37c62a36f83d5ded
ganm0r <gndhrvmv@gmail.com>
2025-01-10 00:49:45 +0530

    initial solution for counting prefixes

---
+++ count_prefixes.cpp
@@ -0,0 +1,26 @@
+// g++ -std=c++11 -O2 -Wall test.cpp -o test && ./test
+// https://leetcode.com/problems/counting-words-with-a-given-prefix/?envType=daily-question&envId=2025-01-09
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int count_prefixes(vector<string>& w, string p) {
+   int c = 0;
+    for (int i = 0; i < w.size() - 1; i++) {
+        int c1 = 1;
+        for (int j = 0; j < p.size() - 1; j++) {
+            if (j <= w[i].size() && w[i][j] == p[j]) c1++;
+        }
+        if (c1 == p.size()) c++;
+    }
+    return c;
+}
+
+int main() {
+    vector<string> w = { "lewsmb","lewrydnve","lewqqm","lewec","lewn","lewb","lewedb" };
+    string p = "lew";
+    cout << count_prefixes(w, p) << "\n";
+    return 0;
+}
