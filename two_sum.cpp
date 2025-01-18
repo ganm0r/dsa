c4785ef901bc83e9ad9bf211e83611b05a93762d
ganm0r <gndhrvmv@gmail.com>
2025-01-19 00:10:42 +0530

    optimise to linear time

--- two_sum.cpp
+++ two_sum.cpp
@@ -3,8 +3,19 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
+
+vector<int> two_sum_n_time(vector<int>& n, int t) {
+    unordered_map<int, int> m;
+    for (int i = 0; i < n.size(); i++) {
+        if (m.count(t - n[i])) return { m[t - n[i]], i };
+        m[n[i]] = i;
+    }
+
+    return { -1, - 1};
+}
 
 vector<int> two_sum_n2_time(vector<int>& n, int t) {
     for (int i = 0; i < n.size(); i++) {
@@ -19,7 +30,7 @@ vector<int> two_sum_n2_time(vector<int>& n, int t) {
 int main() {
     vector<int> n = { 2,7,11,15 };
     cout << "[ ";
-    for (int i : two_sum_n2_time(n, 9)) {
+    for (int i : two_sum_n_time(n, 9)) {
         cout << i << " ";
     }
     cout << "]" << "\n";
